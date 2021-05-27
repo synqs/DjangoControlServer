d2f39b26cefd6993a862647947541be6a18986db
imdippel <ingrid.dippel@kip.uni-heidelberg.de>
2021-05-27 19:18:34 +0200

    starting the temp_control app

---
+++ urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+	path('temp_control/', include('temp_control.urls')),
+	path('pd_monitor/', include('pd_monitor.urls')),
+	path('admin/', admin.site.urls)
+]
