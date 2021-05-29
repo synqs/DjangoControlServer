4083d4d71c375ac8720f2365ee61fd06f1f06d11
Ingrid Maria Dippel <84181950+imdippel@users.noreply.github.com>
2021-05-29 10:55:56 +0200

    create 'index' view

--- apps/pd_monitor/views.py
+++ apps/pd_monitor/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+  devices = Device.objects.all()
+  context = {'devices' : devices}
+  return render(request, 'pd_monitor/index.html', context)
