1915c1bc65cb195763bb7ee1edd903814b22f5f1
imdippel <ingrid.dippel@kip.uni-heidelberg.de>
2021-05-29 12:19:03 +0200

    move apps

--- DjangoControlServer/settings.py
+++ DjangoControlServer/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    'pd_monitor',
+
+    'apps/pd_monitor',
 ]
 
 MIDDLEWARE = [
--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-	path('temp_control/', include('temp_control.urls')),
-	path('pd_monitor/', include('pd_monitor.urls')),
+	path('temp_control/', include('app/temp_control.urls')),
+	path('pd_monitor/', include('app/pd_monitor.urls')),
 	path('admin/', admin.site.urls)
 ]
