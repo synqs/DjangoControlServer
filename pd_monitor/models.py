dcaa2f486507a0d7b79b1b5513ca8a5b7e410b0e
imdippel <ingrid.dippel@kip.uni-heidelberg.de>
2021-05-29 15:30:58 +0200

    table index format

--- pd_monitor/models.py
+++ pd_monitor/models.py
@@ -7,7 +7,7 @@ class Device(models.Model):
 	description = models.CharField(max_length=100, blank=True)	# add. description eg. 2D-MOT path
 	sleeptime = models.FloatField(default=5)
 
-	ip_address = models.CharField(max_length=20, blank=True)	# device ip
+	ip = models.CharField(max_length=20, blank=True)		# device ip
 	port = models.CharField(max_length=4, blank=True)		# device port (incl. default=80 for YUN ?)
 
 	value = models.FloatField(null=True)				# here, this is a voltage
