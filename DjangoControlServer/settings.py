e9a6692798b4bce095cb8b53cc34fed9ee10d5ab
imdippel <ingrid.dippel@kip.uni-heidelberg.de>
2021-05-31 11:54:29 +0200

    add static

--- DjangoControlServer/settings.py
+++ DjangoControlServer/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'pd_monitor',
+    'pd_monitor.apps.PdMonitorConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+	BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
