5596ddccd5194a829bb3e96b3f0d563170047702
Ingrid Maria Dippel <84181950+imdippel@users.noreply.github.com>
2021-06-06 19:37:17 +0200

    remove non-js index

--- pd_monitor/views.py
+++ pd_monitor/views.py
@@ -13,16 +13,11 @@ def add_device(request):
 	device = Objects
 	new_device_list = Device.objects.all()
 	context = {'device_list' : new_device_list}
-	return render(request, 'pd_monitor/index.html', context)
+	return render(request, 'pd_monitor/device_list.html', context)
 
 def remove_device(request, device_id):
 	device = get_object_or_404(Device, pk=device_id)
 	device.delete()
 	new_device_list = Device.objects.all()
 	context = {'device_list' : new_device_list}
-	return render(request, 'pd_monitor/index.html', context)
-
-def index_js(request):
-	device_list = Device.objects.all()
-	context = {'device_list' : device_list}
-	return render(request, 'pd_monitor/index_js.html')
+	return render(request, 'pd_monitor/device_list.html', context)
