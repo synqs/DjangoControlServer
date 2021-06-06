ab8a865ed8b31aef6eac483032339fc75e843764
Ingrid Maria Dippel <84181950+imdippel@users.noreply.github.com>
2021-06-06 19:36:00 +0200

    Update urls.py

--- pd_monitor/urls.py
+++ pd_monitor/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 app_name = 'pd_monitor'
 urlpatterns = [
-	path('', views.index, name='index'),
-	path('<int:device_id>/', views.detail, name='detail'),
-	path('<int:device_id>/remove_device/', views.remove_device, name='remove_device'),
-	path('index_js/', views.index_js, name='index_js')
+	path('', views.IndexView.as_view(), name='index'),
+	path('<int:device_id>/', views.DetailView.as_view(), name='detail'),
+	path('<int:device_id>/remove_device/', views.remove_device, name='remove_device')
 ]
