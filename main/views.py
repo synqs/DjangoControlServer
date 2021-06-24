5287d4d087bcec9dcff2487df9a941d92498ea9d
imdippel <ingrid.dippel@kip.uni-heidelberg.de>
2021-06-24 17:28:26 +0200

    24/06/2021-17:28

--- main/views.py
+++ main/views.py
@@ -12,11 +12,11 @@ def index(request):
 def device_index(request):
 	pdmons = PDmon.objects.all()
 	tctrls = Tctrl.objects.all()
-	res = serializers.serialize('json',list(chain(pdmons, tctrls)))
+	index = serializers.serialize('json',list(chain(pdmons, tctrls)))
 	
-	return HttpResponse(res)
+	return HttpResponse(index)
 
-def device_detail(request, device_name):
-	device_detail_json = get_object_or_404(PDmon, name=device_name)
-	context = { 'device_detail_json' : device_detail_json }
-	return HttpResponse(device_detail_json, content_type="application/json")
+def device_detail(request, device_type, device_name):
+	device = get_object_or_404(device_type, name=device_name)
+	detail = { 'device_detail_json' : device_detail_json }
+	return HttpResponse(device)
