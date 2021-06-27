3f97d266e691ba1e7d79e241b8cad2c5880fc75d
imdippel <ingrid.dippel@kip.uni-heidelberg.de>
2021-06-27 14:05:39 +0200

    27/06/2021-14:05

--- DjangoControlServer/settings.py
+++ DjangoControlServer/settings.py
@@ -132,5 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Allow cross-site references
 
-SESSION_COOKIE_SAMESITE = 'None'
-CSRF_COOKIE_SAMESITE = 'None'
+# SESSION_COOKIE_SAMESITE = 'None'
+# CSRF_COOKIE_SAMESITE = 'None'
--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'main'
 urlpatterns = [
 	path('index/', views.index, name='index'),
-	// path('devices/', views.device_index, name='device_index'),
+	# path('devices/', views.device_index, name='device_index'),
 	path('<str:device_type>/<str:device_name>/', views.detail, name='detail'),
 ]
