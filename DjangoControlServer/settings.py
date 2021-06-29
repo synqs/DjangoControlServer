afaf7155aa1cc3d6421d5f5186d87a2a4634122d
imdippel <ingrid.dippel@kip.uni-heidelberg.de>
2021-06-29 12:03:48 +0200

    29/06/2021-12:03

--- DjangoControlServer/settings.py
+++ DjangoControlServer/settings.py
@@ -119,10 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/main/static/'
 
 STATICFILES_DIRS = [
-	BASE_DIR / 'static',
+	BASE_DIR / '/static/',
 ]
 
 # Default primary key field type
