d31f4304f734aeb63cc0da7861e41e4b2f621427
naka_labpc <82974792+ydhdb@users.noreply.github.com>
2021-12-13 13:45:28 +0100

    moku view

--- apps/mokugo/views.py
+++ apps/mokugo/views.py
@@ -8,39 +8,49 @@ from .models import mokugo
 
 import json
 from pathlib import Path
+from moku.instruments import ArbitraryWaveformGenerator
 
 # Create your views here.
 class MokugoDetailView(DetailView):
-    model = mokugo
-    slug_url_kwarg = 'mokugo_name'
-    slug_field = 'name'
-    template_name = 'mokugo/mokugo.html'
+	model = mokugo
+	slug_url_kwarg = 'mokugo_name'
+	slug_field = 'name'
+	template_name = 'mokugo/mokugo.html'
 
-    def get_context_data(self, **kwargs):
-        Mokugo = super().get_object()
-        context = {}
-        context['mokugo'] = json.loads(serializers.serialize('json', [Mokugo]))[0]['fields']
-        context['mokugo']['model'] = 'mokugo'
-        return context
+	def get_context_data(self, **kwargs):
+		Mokugo = super().get_object()
+		context = {}
+		context['mokugo'] = json.loads(serializers.serialize('json', [Mokugo]))[0]['fields']
+		context['mokugo']['model'] = 'mokugo'
+		return context
 	
 class MokugoDataView(DetailView):
 	model = mokugo
-	
+
 	def get_context_data(self, **kwargs):
+		response = { 'value' : ''}
 		Mokugo = super().get_object()
-		i = AbritrarySignalGenerator()
 		print(self.object)
-		full_path = Path(Path.home().as_posix()+'/Dropbox (CoQuMa)/LabNotes/NaKa/'+timestamp[:7]+'/'+timestamp[:10]+'/data')
+		print(Mokugo)
 
-		try :
-			full_path.mkdir(parents=True, exist_ok=True)
-		except FileExistsError :
-			print('already exists!')
-			full_path = Path(Path.cwd().as_posix()+'/data')
+		try:
+			i = AbritrarySignalGenerator(Mokugo['ip'], force_connect=True)
+		except Exception as e:
+			response['message'] = str(e)
+		else:
+			offset = np.round(i.read_power_supply(1)['set_voltage'],3)
+			response['message'] = 'Data available.'
+			response['value']['offset'] = str(offset)
 
-		with open(str(full_path)+'\\'+kwargs['laser_name']+'_'+timestamp[:10]+'.csv', 'a', newline='', encoding='UTF8') as f:
-			writer = csv.writer(f)
-			writer.writerow([value for key, value in response['value'].items()])
-			f.close()
+			full_path = Path(Path.home().as_posix()+'/Dropbox (CoQuMa)/LabNotes/NaKa/'+timestamp[:7]+'/'+timestamp[:10]+'/data')
+			try :
+				full_path.mkdir(parents=True, exist_ok=True)
+			except FileExistsError :
+				print('already exists!')
+				full_path = Path(Path.cwd().as_posix()+'/data')
+			with open(str(full_path)+'\\'+kwargs['laser_name']+'_'+timestamp[:10]+'.csv', 'a', newline='', encoding='UTF8') as f:
+				writer = csv.writer(f)
+				writer.writerow([value for key, value in response['value'].items()])
+				f.close()
 
 		return HttpResponse(json.dumps(response))
